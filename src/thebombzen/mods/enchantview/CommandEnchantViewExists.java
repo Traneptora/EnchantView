4cbfa2c4107562eae81be29855faa95bae8734a5
Leo Izen <leo.izen@gmail.com>
2015-07-22 23:49:16 -0400

    Updated CommandEnchantViewExists to 1.8

--- src/thebombzen/mods/enchantview/CommandEnchantViewExists.java
+++ src/thebombzen/mods/enchantview/CommandEnchantViewExists.java
@@ -1,5 +1,8 @@
 package thebombzen.mods.enchantview;
 
+import java.util.Collections;
+import java.util.List;
+
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -8,7 +11,7 @@ import net.minecraft.util.IChatComponent;
 public class CommandEnchantViewExists extends CommandBase {
 
 	@Override
-	public boolean canCommandSenderUseCommand(ICommandSender par1ICommandSender) {
+	public boolean canCommandSenderUse(ICommandSender par1ICommandSender) {
 		if (par1ICommandSender instanceof EntityPlayerMP) {
 			return EnchantView.sideSpecificUtilities.canPlayerUseCommand((EntityPlayerMP) par1ICommandSender);
 		} else {
@@ -17,8 +20,8 @@ public class CommandEnchantViewExists extends CommandBase {
 	}
 
 	@Override
-	public String getCommandName() {
-		return "doesenchantviewexist";
+	public List<String> getAliases() {
+		return Collections.singletonList("doesenchantviewexist");
 	}
 
 	@Override
@@ -32,8 +35,14 @@ public class CommandEnchantViewExists extends CommandBase {
 	}
 
 	@Override
-	public void processCommand(ICommandSender sender, String[] strings) {
-		sender.addChatMessage(IChatComponent.Serializer.func_150699_a("{\"text\":\"Yes, EnchantView exists.\"}"));
+	public void execute(ICommandSender sender, String[] strings) {
+		sender.addChatMessage(IChatComponent.Serializer.jsonToComponent("{\"text\":\"Yes, EnchantView exists.\"}"));
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
