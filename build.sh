27ceb3d4b3178577704b75c3cf37041e8ab9ffff
Leo Izen <leo.izen@gmail.com>
2016-05-02 23:17:41 -0400

    Final 1.8.9 commit

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+
+function moveback {
+	mv -v ./bukkit src/main/java/thebombzen/mods/enchantview/bukkit
+}
+trap moveback EXIT
 
 VERS=5.0.1
-MC_VERS=1.8
+MC_VERS=1.8.9
 BASENAME=enchantview
 LONGNAME=EnchantView
 SHORTNAME=EV
 
 cd $(dirname $0)
 ARCH=$LONGNAME-v$VERS-mc$MC_VERS.jar
-mv src/main/java/thebombzen/mods/enchantview/bukkit ./bukkit
+mv -v src/main/java/thebombzen/mods/enchantview/bukkit ./bukkit
 ./gradlew build
-FAIL=$?
-mv ./bukkit src/main/java/thebombzen/mods/enchantview/bukkit
-if [ $FAIL -ne 0 ]; then exit $FAIL; fi
 
-cp build/libs/$BASENAME-$VERS.jar $ARCH
-mkdir -p META-INF
+cp -v build/libs/$BASENAME-$VERS.jar $ARCH
+mkdir -v -p META-INF
 
 cat >META-INF/MANIFEST.MF <<EOF
 Manifest-Version: 1.0
